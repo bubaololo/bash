d0fdbe641ad6a055f1a8a16c4dff492565d07419
bubaololo <79483014+bubaololo@users.noreply.github.com>
2022-06-14 01:15:41 +0200

    Update up.sh

--- up.sh
+++ up.sh
@@ -3,13 +3,10 @@
 PASSWORD="changeme"
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt install nano -y
-sudo apt install mc -y
-sudo apt install htop -y
+apt-get -y install curl nano mc openssl git htop zip unzip memcached
+apt-get memcached redis mysql git
 sudo apt install php php-common php-fpm php-cli php-curl php-gd php-mysql php-imagick php-mbstring php-xml phpunit -y
 php -r "readfile('https://getcomposer.org/installer');" | php
-sudo apt install mysql -y
-sudo apt install git -y
 sudo apt-get install ufw -y
 sudo ufw allow 3389
 sudo apt install xrdp 
